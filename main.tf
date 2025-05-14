4d3608d39bf850ce3d19c012a0cda8b42be44000
Cesar Armengol <56865186+drus062@users.noreply.github.com>
2025-05-14 10:42:24 +0100

    Vale

--- main.tf
+++ main.tf
@@ -11,14 +11,14 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = ["940482406080"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HolaMundo"
   }
 }
