6fb2d583508356f2684b7a35ef9871909482c2e1
Cesar Armengol <56865186+drus062@users.noreply.github.com>
2025-05-15 18:09:24 +0100

    NewVpC

--- main.tf
+++ main.tf
@@ -14,11 +14,28 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id = module.blog_vpc.public_subnets[0]
+  
   tags = {
-    Name = "HolaMundo!"
+    Name = "HolaKabrones!!"
+  }
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
